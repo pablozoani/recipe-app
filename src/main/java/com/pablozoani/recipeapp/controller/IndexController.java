98132153142f13d5b68ea6804285d3b141f6ddef
pablozoani <pablozoani@hotmail.com>
2020-06-09 05:25:23 -0300

    Initial commit

---
+++ src/main/java/com/pablozoani/recipeapp/controller/IndexController.java
@@ -0,0 +1,7 @@
+package com.pablozoani.recipeapp.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+}
---
+++ src/test/java/com/pablozoani/recipeapp/RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablozoani.recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
