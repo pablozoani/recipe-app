7efefff89c279d89c531c2354d78374795374eb8
pablozoani <pablozoani@hotmail.com>
2020-06-22 11:44:14 -0300

    initialized integration test, added maven fail safe plugin.

---
+++ src/test/java/com/pablozoani/recipeapp/repository/UnitOfMeasureRepositoryIntegrationTest.java
@@ -0,0 +1,36 @@
+package com.pablozoani.recipeapp.repository;
+
+import com.pablozoani.recipeapp.model.UnitOfMeasure;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+class UnitOfMeasureRepositoryIntegrationTest {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findByUnitOfMeasure() {
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByUnitOfMeasure("Cups");
+        assertEquals("Cups", unitOfMeasureOptional.get().getUnitOfMeasure());
+    }
+}
